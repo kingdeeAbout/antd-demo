b017b21a84898293b52ac485afcbbe21b5f04798
dadawangweb <1024564775@qq.com>
2018-02-01 18:25:30 +0800

    react begin
    todo：
    exp：
    ques: 无

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Button from 'antd/lib/button';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Button type="primary">Button</Button>
+        </div>
+    );
+  }
+}
+
+export default App;
